e4f3bc85122cbbeea8a115819e1f43a345e662ba
Eric Denovellis <edeno@bu.edu>
2019-10-03 09:53:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy >= 1.11', 'pandas']
+INSTALL_REQUIRES = ['numpy', 'pandas']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='rec_to_binaries',
-    version='0.1.0.dev0',
+    version='0.1.1.dev0',
     license='MIT',
     description=('Covert SpikeGadgets rec files to binaries'),
-    author='Eric Denovellis',
+    author='Eric Denovellis, Daniel Liu',
     author_email='Eric.Denovellis@ucsf.edu',
-    url='https://github.com/LorenFrankLab/rec_to_binaries',
+    url='https://github.com/edeno/rec_to_binaries',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
+    python_requires='>3.6',
     tests_require=TESTS_REQUIRE,
 )
