0ab82009875687c3e0bfbcb2e0391e06849f5ce0
Eric Denovellis <edeno@bu.edu>
2020-02-28 10:13:55 -0800

    Bump version number.

--- setup.py
+++ setup.py
@@ -7,11 +7,7 @@ TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='rec_to_binaries',
-<<<<<<< HEAD
-    version='0.1.2.dev0',
-=======
-    version='0.2.1.dev0',
->>>>>>> 963eee1406be2c213649bedd2819001df79ae8de
+    version='0.2.2.dev0',
     license='MIT',
     description=('Covert SpikeGadgets rec files to binaries'),
     author='Eric Denovellis, Daniel Liu',
@@ -19,6 +15,6 @@ setup(
     url='https://github.com/LorenFrankLab/rec_to_binaries',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
-    python_requires='>3.6',
+    python_requires='>=3.6',
     tests_require=TESTS_REQUIRE,
 )
