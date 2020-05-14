dc288e5b59542c79c8858cf7df9c9ad8fb1db916
Eric Denovellis <edeno@bu.edu>
2020-05-14 13:34:39 -0700

    Add scipy to dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy', 'pandas']
+INSTALL_REQUIRES = ['numpy', 'pandas', 'scipy']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
