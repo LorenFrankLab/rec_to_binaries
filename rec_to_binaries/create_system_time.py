ff02660dc81efe3f65bea9cf329a12cebdf5af54
Eric Denovellis <edeno@bu.edu>
2020-05-18 16:28:48 -0700

    Minor formatting.

--- rec_to_binaries/create_system_time.py
+++ rec_to_binaries/create_system_time.py
@@ -1,6 +1,8 @@
-from read_binaries import readTrodesExtractedDataFile, write_trodes_extracted_datafile
-
 import numpy as np
+
+from read_binaries import (readTrodesExtractedDataFile,
+                           write_trodes_extracted_datafile)
+
 
 def add_system_time_to_file(continuous_time_file, new_file_name):
 	cont_time = readTrodesExtractedDataFile(continuous_time_file)
@@ -11,38 +13,44 @@ def add_system_time_to_file(continuous_time_file, new_file_name):
 		cont_time['Fields'] = '<trodestime uint32><systime int64>'
 	write_trodes_extracted_datafile(new_file_name, cont_time)
 
+
 def create_and_add_system_timepoints(cont_time):
 	# Assumptions: 'clockrate' is in Hz; 'system_time_at_creation' is taken to the millisecond
 
 	if 'clockrate' in cont_time:
-		nanosec_per_sample = int((1/int(cont_time['clockrate']))* (1e9))
+		nanosec_per_sample = int((1 / int(cont_time['clockrate'])) * (1e9))
 	else:
-		nanosec_per_sample = int((1/int(cont_time['Clockrate']))* (1e9))
+		nanosec_per_sample = int((1 / int(cont_time['Clockrate'])) * (1e9))
 
 	sys_time = np.zeros(np.shape(cont_time['data'])).astype('int64')
 
 	if 'timestamp_at_creation' in cont_time:
-		interval = (cont_time['data'][0][0] - int(cont_time['timestamp_at_creation'])) * nanosec_per_sample
+		interval = (cont_time['data'][0][0] -
+		            int(cont_time['timestamp_at_creation'])) * nanosec_per_sample
 	else:
-		interval = (cont_time['data'][0][0] - int(cont_time['Timestamp_at_creation'])) * nanosec_per_sample
+		interval = (cont_time['data'][0][0] -
+		            int(cont_time['Timestamp_at_creation'])) * nanosec_per_sample
 
 	if 'system_time_at_creation' in cont_time:
-		sys_time[0] = (interval + (int(cont_time['system_time_at_creation']) * (1e6))).astype(int)
+		sys_time[0] = (
+		    interval + (int(cont_time['system_time_at_creation']) * (1e6))).astype(int)
 	else:
-		sys_time[0] = (interval + (int(cont_time['System_time_at_creation']) * (1e6))).astype(int)
+		sys_time[0] = (
+		    interval + (int(cont_time['System_time_at_creation']) * (1e6))).astype(int)
 
-	for i in range(1,np.size(sys_time)):
-	    trodes_interval = cont_time['data'][i][0] - cont_time['data'][i-1][0]
-	    
-	    #conditionals account for rounding and skipped trodes samples in nanoseconds
-	    if (trodes_interval%3==0):
-	        sys_interval = int(trodes_interval * nanosec_per_sample) + int(trodes_interval/3)
-	    elif (i%3==0):
+	for i in range(1, np.size(sys_time)):
+	    trodes_interval = cont_time['data'][i][0] - cont_time['data'][i - 1][0]
+
+	    # conditionals account for rounding and skipped trodes samples in nanoseconds
+	    if (trodes_interval % 3 == 0):
+	        sys_interval = int(trodes_interval *
+	                           nanosec_per_sample) + int(trodes_interval / 3)
+	    elif (i % 3 == 0):
 	        sys_interval = int(trodes_interval * nanosec_per_sample) + 1
 	    else:
 	        sys_interval = int(trodes_interval * nanosec_per_sample)
-	    
-	    sys_time[i] = sys_interval + sys_time[i-1]
+
+	    sys_time[i] = sys_interval + sys_time[i - 1]
 
     return package_sys_time_with_trodes_time(sys_time, cont_time)
 
