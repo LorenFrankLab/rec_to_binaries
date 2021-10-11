cd2272a463fa6d3edaf3904b844881840b26abc5
Jai Yu <jaiyu@uchicago.edu>
2021-10-10 20:43:19 -0500

    change sorting mode to 0

--- rec_to_binaries/trodes_data.py
+++ rec_to_binaries/trodes_data.py
@@ -1407,7 +1407,7 @@ class ExtractRawTrodesData:
                     export_call.extend(export_args)
                     
                     # add sorting mode
-                    export_call += ['-sortingmode', '1']
+                    export_call += ['-sortingmode', '0']
 
                     export_call += ['-rec', file_path,
                                     '-outputdirectory', out_date_dir,
