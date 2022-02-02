e38e94e662c163ddafd5c951b5b1e13f5b89a3a8
Eric Denovellis <edeno@bu.edu>
2022-02-02 15:38:41 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
     name='rec_to_binaries',
-    version='0.7.1',
+    version='0.7.2',
     license='MIT',
     description=('Covert SpikeGadgets rec files to binaries'),
     author='Eric Denovellis, Daniel Liu',
