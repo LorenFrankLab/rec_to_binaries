040293c2b8614caa7540a9358e73c24c3108e355
Philip Adenekan <zoldello@greenhouse.cin.ucsf.edu>
2022-02-03 12:14:05 -0800

    some cleanup on environment.yml

--- rec_to_binaries/test/e2etests/test_setup.py
+++ rec_to_binaries/test/e2etests/test_setup.py
@@ -9,19 +9,13 @@ def test_check_spikegadget_is_installed():
     assert spike_gadget_exists
 
 def test_check_if_rec_to_binaries_is_installed():
-    package = None
-    try:
-        package = pkg_resources.get_distribution('rec_to_binaries')
-    except pkg_resources.DistributionNotFound:
-        pass
+    package = pkg_resources.get_distribution('rec_to_binaries')
+
     assert package is not None
 
 def test_major_mountainlab_pytools_version_number_is_useable():
-    package = None
-    try:
-        package = pkg_resources.get_distribution('mountainlab-pytools')
-    except pkg_resources.DistributionNotFound:
-        pass
+    package = pkg_resources.get_distribution('mountainlab-pytools')
+
     assert package is not None
     assert package.version.split('.')[0] == '0', 'Major semver update, smoke test code'
     assert package.version.split('.')[1] >= '7', 'Minor semver downgrade, check installation'
